c673daf2aa039550d07a91064b0dfce5a66d722a
Cherry-Machado <Cherry5467@gmail.com>
2025-05-13 02:40:52 -0300

    Finishing week1 project

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-//router.get('/', (req, res) => {res.send('Hello World!')});
+router.get('/', (req, res) => {res.send('Welcome to my Contacts Project!')});
 
 router.use('/contacts', require('./contacts'));
 
