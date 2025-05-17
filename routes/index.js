c472a7736a8f19e97d3f10ae740f6fad5cfe07a0
Cherry-Machado <Cherry5467@gmail.com>
2025-05-17 02:38:54 -0300

    Updating router > index.js

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 router.use('/', require('./swagger'));
 
 //#swagger.tags=["Hello World"];
-router.get('/', (req, res) => {res.send('Welcome to my Contacts Project!')});
+//router.get('/', (req, res) => {res.send('Welcome to my Contacts Project!')});
 
 router.use('/contacts', require('./contacts'));
 
