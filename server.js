da65868219c653c6c49e168a9fd3fd32158cc519
Cherry-Machado <Cherry5467@gmail.com>
2025-05-17 01:42:14 -0300

    Installed Swagger

--- server.js
+++ server.js
@@ -8,6 +8,21 @@ require('dotenv').config();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Add support for CORS
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, PUT, DELETE, OPTIONS'
+  );
+  next();
+});
+
 app.use('/', require('./routes')); 
 
 const port = process.env.PORT || 3000; // Fallback to 3000 if no env var
