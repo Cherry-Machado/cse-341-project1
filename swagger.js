2c76ed3a684beff1977373c7fe92bcfe22322855
Cherry-Machado <Cherry5467@gmail.com>
2025-05-17 02:08:04 -0300

    Updating

--- swagger.js
+++ swagger.js
@@ -13,9 +13,4 @@ const outputFile = './swagger.json';
 const endpointsFiles = ['./routes/index.js'];
 
 // generate swagger.json
-//swaggerAutogen(outputFile, endpointsFiles, doc);
-
-// Run server after it gets generated
-swaggerAutogen(outputFile, endpointsFiles, doc).then(async () => {
-  await import('./server.js');
- });
+swaggerAutogen(outputFile, endpointsFiles, doc);
